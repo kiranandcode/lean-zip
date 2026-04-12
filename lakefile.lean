c1e51fb6f4e98d649b392fcfa57aa129b735febe
Kiran Gopinathan <kiran@basis.ai>
2026-04-12 15:29:00 -0400

    pruned

--- lakefile.lean
+++ lakefile.lean
@@ -2,21 +2,10 @@ import Lake
 open System Lake DSL
 
 package «lean-zip» where
-  testDriver := "test"
 
 require zipCommon from git "https://github.com/kim-em/lean-zip-common" @ "87480b0"
 
 lean_lib Zip
-
-lean_lib ZipTest where
-  globs := #[.submodules `ZipTest]
-
-@[default_target]
-lean_exe test where
-  root := `ZipTest
-
-lean_exe bench where
-  root := `ZipBench
 
 lean_exe «lean-zip» where
   root := `ZipCli
